deb1eaba369d9d3c4ef85acef709908d09fd5609
trefynwynd <jl.alejandria@gmail.com>
2020-10-05 23:19:46 -0700

    moved js code from style.css to here.

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+console.log("Insanity Check!")
+
+// By using the getCards function from High-Card, create an array of four different cards.
+// By using the shuffleCards function from High-Card, splay out the board. First as a 2x2 board, then a 2x4 board to complete the MVP.
+// The PokeASquare part of the event listeners will be useful for waiting on the clicks.
