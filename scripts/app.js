70c6c5760a8b5d2d7fe30ec5fa84d5f1d4ca69be
trefynwynd <jl.alejandria@gmail.com>
2020-10-06 21:41:30 -0700

    Started on a function and a button.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
 console.log("Insanity Check!")
 
 // By using the getCards function from High-Card, create an array of four different cards.
-// By using the shuffleCards function from High-Card, splay out the board. First as a 2x2 board, then a 2x4 board to complete the MVP.
+// By using the shuffleCards function from High-Card, this will dictate what m8s are under each card.
 // The PokeASquare part of the event listeners will be useful for waiting on the clicks.
+
+// First stage: Create the first grid of 2x2, setup the spacing and background. If not a picture, then a tiled background, if not then blue.
+
+function readyCards() {
+    // When the begin button is pressed, the board is revealed.
+    for (let i = 0; i <= 4; i++) {
+        // The cards are spread out onto the board. To create a second row, a wrap will occur.
+    }
+}
+
+// board element
+// cards element
+// Begin button element
+let beginButton = document.createElement('button')
+beginButton.setAttribute("class", "beginButton")
+document.section.appendChild("beginButton")
+// Difficulty button element
